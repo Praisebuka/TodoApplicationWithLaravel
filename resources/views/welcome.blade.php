e6c5a763bee388fd79d1f3b4e1751b513f5d7fd6
Praise Ebuka <praise.njoga@gmail.com>
2023-01-27 19:34:50 +0100

    Done with the home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,7 +16,46 @@
             }
         </style>
     </head>
-    <body>
-    
+    <body class="bg-gray-200 p-4">
+        <div class="lg:w-2/4 mx-auto py-8 px-6 bg-white rounded-xl">
+            <h1 class="font-bold text-5xl text-center mb-8">
+                Todo App with Laravel
+            </h1>
+
+            <div class="mb-6">
+                <form class="flex flex-col space-y-4" method="POST" action="/">
+                    <input type="text" name="title" placeholder="theTodoTitle" class="py-3 px-4 bg-gray-100 rounded-xl">
+                    
+                    <textarea name="description" placeholder="the description of your Todo" class="py-3 px-4 bg-gray-100 rounded-xl"></textarea>
+                    <button class="w-40 py-4 px-8 bg-green-500 text-white rounded-xl">
+                        Add-Items
+                    </button>
+                </form>
+            </div>
+
+            <hr>
+             <div class="mt-2">
+                <div class="p-4 flex items-center border-b border-gray-300 px-3">
+                    <div class="flex-1 pr-8">
+                        <h3 class="text-lg font-semibold">The Title</h3>
+                        <p class="text-gray-500">The description</p>
+                    </div>
+
+                    <div class="flex space-x-3">
+                        <button class="py-2 px-2 bg-green-500 text-white rounded-xl">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 01-1.043 3.296 3.745 3.745 0 01-3.296 1.043A3.745 3.745 0 0112 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 01-3.296-1.043 3.745 3.745 0 01-1.043-3.296A3.745 3.745 0 013 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 011.043-3.296 3.746 3.746 0 013.296-1.043A3.746 3.746 0 0112 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 013.296 1.043 3.746 3.746 0 011.043 3.296A3.745 3.745 0 0121 12z" />
+                              </svg>
+                        </button>
+
+                        <button class="py-2 px-2 bg-red-500 text-white rounded-xl">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+             </div>
+        </div>
     </body>
 </html>
