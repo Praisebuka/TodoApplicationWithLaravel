19d14ff967b198f581996b69fd273320c3cf1916
Praise Ebuka <praise.njoga@gmail.com>
2023-11-13 13:57:15 +0100

    okayy, what's up?

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -56,41 +56,55 @@
 
              <div class="mt-2">
                 @foreach ($todos as $todo)
-                    <div 
-                        @class([
+                    <div @class([
                             'p-4 flex items-center border-b border-gray-300 px-3',
                             $todo->isDone ? 'bg-green-200' : ''
-                        ])
-                    >
+                        ])>
+
                         <div class="flex-1 pr-8">
                             <h3 class="text-lg font-semibold">{{ $todo->title }}</h3>
                             <p class="text-gray-500">{{ $todo->description }}</p>
                         </div>
 
                         <div class="flex space-x-3">
+
                             <form method="POST" action="/{{ $todo->id }}">
                                 @csrf
                                 @method('PATCH')
                                 <button class="py-2 px-2 bg-green-500 text-white rounded-xl">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6" title="done">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 01-1.043 3.296 3.745 3.745 0 01-3.296 1.043A3.745 3.745 0 0112 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 01-3.296-1.043 3.745 3.745 0 01-1.043-3.296A3.745 3.745 0 013 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 011.043-3.296 3.746 3.746 0 013.296-1.043A3.746 3.746 0 0112 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 013.296 1.043 3.746 3.746 0 011.043 3.296A3.745 3.745 0 0121 12z" />
                                     </svg>
                                 </button>
+
+                                @method('PUT')
+                                <button class="py-2 px-2 bg-blue-500 text-white rounded-xl">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-6 h-6" title="edit">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l2 2-2 2M4 11a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2H6a2 2 0 01-2-2v-8z"></path>
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l2 2-2 2"></path>
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 3v4a1 1 0 001 1h4a1 1 0 001-1V3a2 2 0 00-2-2h-1a2 2 0 00-2 2z"></path>
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 8l-6 6"></path>
+                                    </svg>
+                                </button>
+                                
                             </form>
 
                             <form method="POST" action="/{{ $todo->id }}">
                                 @csrf
                                 @method('DELETE')
                                 <button class="py-2 px-2 bg-red-500 text-white rounded-xl">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6" title="delete">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                                     </svg>
                                 </button>
                             </form>
+
                         </div>
                     </div>
                 @endforeach
              </div>
+
+
         </div>
     </body>
 </html>
