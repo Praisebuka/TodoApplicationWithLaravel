16845cf42b2204233b40fd3d1fe5627fb19f3af7
Praise Ebuka <praise.njoga@gmail.com>
2023-11-14 19:25:46 +0100

    adjusted home page (put and patch) methods

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -76,7 +76,9 @@
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 01-1.043 3.296 3.745 3.745 0 01-3.296 1.043A3.745 3.745 0 0112 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 01-3.296-1.043 3.745 3.745 0 01-1.043-3.296A3.745 3.745 0 013 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 011.043-3.296 3.746 3.746 0 013.296-1.043A3.746 3.746 0 0112 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 013.296 1.043 3.746 3.746 0 011.043 3.296A3.745 3.745 0 0121 12z" />
                                     </svg>
                                 </button>
+                            </form>
 
+                            <form method="POST" action="/{{ $todo->id }}">
                                 @method('PUT')
                                 <button class="py-2 px-2 bg-blue-500 text-white rounded-xl">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-6 h-6" title="edit">
@@ -86,7 +88,6 @@
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 8l-6 6"></path>
                                     </svg>
                                 </button>
-                                
                             </form>
 
                             <form method="POST" action="/{{ $todo->id }}">
