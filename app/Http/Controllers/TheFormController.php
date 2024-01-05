f7bd3d51ad887926aad983c3bc64ed634994c863
Praise Ebuka <praise.njoga@gmail.com>
2024-01-05 20:28:41 +0100

    we all got updated :)

--- app/Http/Controllers/TheFormController.php
+++ app/Http/Controllers/TheFormController.php
@@ -11,9 +11,12 @@ class TheFormController extends Controller
     {
         # Showing what was stored in the database
         $todos = theForm::all();
+        $done = 'Mark this todo as done';
+        $edit = 'Click this to edit this item';
+        $delete = 'Click this icon if you want to delete this todo item';
 
         # Taking the created Todos from the user 
-        return view('welcome', [ 'todos' => $todos ]);
+        return view('welcome', [ 'todos' => $todos, 'done' => $done, 'edit' => $edit, 'delete' => $delete ]);
     }
 
     public function store() 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,7 @@
         <style>
             body {
                 font-family: 'Nunito', sans-serif;
-                /* background-image: url('/images/nice.png'); */
+                background-image: url('/images/background.jpg');
                 background-repeat: no-repeat;
                 width: 100%;
                 height: 100vh;
@@ -30,7 +30,7 @@
 
         <img src="/images/background.jpg" alt="icon_image" width="60px" height="60px">
 
-        <div class="lg:w-2/3 mx-auto py-8 px-6 bg-white rounded-xl mt-12">
+        <div class="lg:w-2/4 mx-auto py-8 px-6 bg-white rounded-xl mt-1">
             <h1 class="gradient-text font-bold text-5xl text-center mb-8">
                 Todo App with Laravel
             </h1>
@@ -43,8 +43,8 @@
                     
                     <textarea name="description" placeholder="Your Todo description" class="py-3 px-4 bg-gray-100 rounded-xl"></textarea>
                     
-                    <div class="text-center text-2xl">
-                        <button class="w-64 h-20 py-4 px-8 bg-green-500 text-white rounded-xl font-bold">
+                    <div class="text-center text-xl">
+                        <button class="w-64 h-16 py-4 px-8 bg-green-500 text-white rounded-xl font-bold">
                             Add-Items
                         </button>
                     </div>
@@ -71,7 +71,7 @@
                             <form method="POST" action="/{{ $todo->id }}">
                                 @csrf
                                 @method('PATCH')
-                                <button class="py-2 px-2 bg-green-500 text-white rounded-xl">
+                                <button class="py-2 px-2 bg-green-500 text-white rounded-xl" title="{{ $done }}">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6" title="done">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 01-1.043 3.296 3.745 3.745 0 01-3.296 1.043A3.745 3.745 0 0112 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 01-3.296-1.043 3.745 3.745 0 01-1.043-3.296A3.745 3.745 0 013 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 011.043-3.296 3.746 3.746 0 013.296-1.043A3.746 3.746 0 0112 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 013.296 1.043 3.746 3.746 0 011.043 3.296A3.745 3.745 0 0121 12z" />
                                     </svg>
@@ -80,7 +80,7 @@
 
                             <form method="POST" action="/{{ $todo->id }}">
                                 @method('PUT')
-                                <button class="py-2 px-2 bg-blue-500 text-white rounded-xl">
+                                <button class="py-2 px-2 bg-blue-500 text-white rounded-xl" title="{{ $edit }}">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-6 h-6" title="edit">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l2 2-2 2M4 11a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2H6a2 2 0 01-2-2v-8z"></path>
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l2 2-2 2"></path>
@@ -93,7 +93,7 @@
                             <form method="POST" action="/{{ $todo->id }}">
                                 @csrf
                                 @method('DELETE')   
-                                <button class="py-2 px-2 bg-red-500 text-white rounded-xl">
+                                <button class="py-2 px-2 bg-red-500 text-white rounded-xl" title="{{ $delete }}">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6" title="delete">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                                     </svg>
